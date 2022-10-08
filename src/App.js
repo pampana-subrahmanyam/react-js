5c2b4855171231f424fe35df7b1748d3e763ad41
SubbuPampana <subrahmanyam@jungleegames.com>
2022-10-08 11:54:46 +0530

    working state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+////IN react js ,we can create componect by using functions
+function getLogo(){
+  let wishName="Subbu"
+  ///This is a simple component that return Logo
+  return (<div>
+<h1>{wishName}</h1>
+<img src={logo} className="App-logo" alt="logo" />
+
+  </div>);
+}
 function App() {
+  return getLogo();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
